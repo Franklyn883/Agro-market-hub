f39a6562e194f896b76f530b230cd44fa7041c6c
Frank Alimimian <alimimianfrank@gmail.com>
2024-03-21 20:45:54 +0100

    Add token authentication to store this commits add initialization code to store and also store the token in the local storage

--- agro_market_frontend/src/store/index.js
+++ agro_market_frontend/src/store/index.js
@@ -1,9 +1,34 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    user: {
+        username: ''
+    },
+    isAuthenticated: false,
+    token: ''
+  },
   getters: {},
-  mutations: {},
+  mutations: {
+    initializeStore(state){
+        if(localStorage.getItem('token')){
+            state.token = localStorage.getItem(token)
+            state.isAuthenticated = true
+        }
+        else{
+            state.token ="",
+            state.isAuthenticated = false
+        }
+    },
+    setToken(state,token){
+        state.token = token,
+        state.isAuthenticated = true
+    },
+    removeToken(state){
+        state.token = '',
+        state.isAuthenticated = false
+    }
+  },
   actions: {},
   modules: {},
 });
